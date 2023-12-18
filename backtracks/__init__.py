37a40373019a1f118be965713868c1863bbef21a
Tomas Stolker <stolker@strw.leidenuniv.nl>
2023-12-18 18:54:01 +0100

    Fixed issue with corr_terms and confidence envelopes, and more (#23)
    * Fixed issue with use of corr_terms in likelihood function, fixed issue with confidence envelopes in trackplot, added posterior samples to deltaRA-deltaDEC plot, removed the jd_tt parameter and added the ref_epoch parameter to generate_plots, added fileprefix parameter to system class, simplified the extraction of samples by using samples_equal from dynesty, added Github Actions workflow, updated .gitignore, updated package __init__, added unit tests to tests folder, removed all binary files that are not required by the repo, tested compatibility with Python 3.11
    
    * Install numpy separately before orbitize
    
    * Testing installation of dependencies
    
    * Git clone orbitize
    
    * Updated with installing orbitize from Github
    
    * Fixing pip install of orbitize
    
    * Testing Github workflow
    
    * Testing workflow
    
    * Skip dependencies
    
    * Testing installation
    
    * Fixed issue in main.yml
    
    * More testing...
    
    * Even more testing...
    
    * Added requirements without orbitize to workflow folder
    
    * Testing
    
    * Testing
    
    * Added requirements file
    
    * Added import sys
    
    * Fixed issue with data_times
    
    * Fixed minor issue
    
    * Further testing
    
    * Fixing PYTHONPATH
    
    * Added orbitize requirements
    
    * Minor fix

---
+++ backtracks/__init__.py
@@ -0,0 +1,8 @@
+from backtracks.backtracks import system
+
+__author__ = "William Balmer"
+__license__ = "BSD-3"
+__version__ = "0.3.1"
+__maintainer__ = "William Balmer"
+__email__ = "wbalmer1@jhu.com"
+__status__ = "Development"
---
+++ tests/test_backtracks.py
@@ -0,0 +1,65 @@
+import os
+import pytest
+
+from backtracks import system
+
+class TestBacktracks:
+    def setup_class(self) -> None:
+
+        self.test_dir = os.path.dirname(__file__) + "/"
+
+        self.bt_system = system(
+            "HD 131399 A",
+            f"{self.test_dir}scorpions1b_orbitizelike.csv",
+            nearby_window=0.5,
+            fileprefix=self.test_dir,
+        )
+
+    def teardown_class(self) -> None:
+
+        file_list = [
+            "HD_131399_A_bjprior_backtracks.png",
+            "HD_131399_A_corner_backtracks.png",
+            "HD_131399_A_evidence_backtracks.png",
+            "HD_131399_A_model_backtracks.png",
+            "HD_131399_A_nearby_gaia_distribution.png",
+            "HD_131399_A_dynestyrun_results.pkl",
+            "gaia_query_6204835284262018688.fits",
+            "dynesty.save"
+        ]
+
+        for file_item in file_list:
+            if os.path.exists(self.test_dir + file_item):
+                os.remove(self.test_dir + file_item)
+
+    def test_system(self) -> None:
+
+        assert isinstance(self.bt_system, system)
+
+    def test_fit(self) -> None:
+
+        results = self.bt_system.fit(dlogz=0.5, npool=4, dynamic=False, mpi_pool=False, nlive=100, resume=False)
+        print(type(results))
+
+        # assert isinstance(results, system)
+
+    def test_save(self) -> None:
+
+        self.bt_system.save_results(fileprefix=self.test_dir)
+
+    def test_load(self) -> None:
+
+        self.bt_system.load_results(fileprefix=self.test_dir)
+
+    def test_plot(self) -> None:
+
+        self.bt_system.generate_plots(
+            days_backward=3.*365.,
+            days_forward=3.*365.,
+            step_size=50.,
+            ref_epoch=None,
+            plot_radec=False,
+            plot_stationary=False,
+            fileprefix=self.test_dir,
+            filepost='.png',
+        )
