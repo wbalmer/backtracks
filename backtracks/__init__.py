b5fea4c45d5a81d5f1045d48ee6dc1a03cf347c6
wbalmer <williamobalmer@gmail.com>
2023-12-21 12:47:30 -0500

    increment version, build pushed to pypi in order to update the docs link

--- backtracks/__init__.py
+++ backtracks/__init__.py
@@ -2,7 +2,7 @@ from backtracks.backtracks import System
 
 __author__ = "William Balmer"
 __license__ = "BSD-3"
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __maintainer__ = "William Balmer"
 __email__ = "wbalmer1@jhu.com"
 __status__ = "Development"
