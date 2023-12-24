6949ede8f551232ebb81f2735d23fe44c8d7195f
Gilles Otten <gilles.otten@gmail.com>
2023-12-24 13:37:10 +0800

    Fixed error in call to cornerplot, also added title_quantiles.

--- backtracks/plotting.py
+++ backtracks/plotting.py
@@ -73,8 +73,9 @@ def posterior(backtracks, fileprefix='./', filepost='.pdf'):
                                max_n_ticks=4,
                                # label_kwargs={},
                                show_titles=True,
-                               title_kwargs={'fontsize':14}
+                               title_kwargs={'fontsize':14},
                                quantiles=(0.1587,0.5,0.8413),
+                               title_quantiles=(0.1587,0.5,0.8413)
                                # fig=(fig, axes[:, 4:])
                               )
     fig.figsize = (9,9)
