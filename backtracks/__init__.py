3a1e002a4527d3a5aaff70b8ab4d393fce1d7800
wbalmer <williamobalmer@gmail.com>
2025-02-28 18:58:09 -0500

    increment version

--- backtracks/__init__.py
+++ backtracks/__init__.py
@@ -2,7 +2,7 @@ from backtracks.backtracks import System
 
 __author__ = "William Balmer"
 __license__ = "BSD-3"
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 __maintainer__ = "William Balmer"
 __email__ = "wbalmer1@jhu.com"
 __status__ = "Development"
